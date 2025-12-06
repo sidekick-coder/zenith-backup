88c96575368a1afa12302d2d018122158058240d
Henrique <henrique@sidekick-coder.com>
2025-12-06 12:53:06 -0300

    feat: module.client.ts

---
+++ client/module.client.ts
@@ -0,0 +1,38 @@
+import { useMenu } from '#client/composables/useMenu.ts'
+import Module from '#client/entities/module.entity.ts'
+import router from '#client/facades/router.facade.ts'
+import authGuard from '#client/guards/auth.guard.ts'
+import { $t } from '#shared/lang.ts'
+
+export default class MyModule extends Module {
+    public async onLoad(): Promise<void> {
+        const menu = useMenu()
+        
+        router.auto(import.meta.glob<any>('./pages/admin/**/*.vue'), {
+            strip: ['pages', 'admin'],
+            prefix: '/admin/zbackup',
+            guards: [authGuard],
+        })
+        
+        router.addRoute({
+            path: '/admin/zbackup',
+            redirect: '/admin/zbackup/plans',
+        })
+
+        menu.add({
+            id: 'zbackup-snapshots',
+            label: $t('Snapshots'),
+            icon: 'DatabaseBackup',
+            group: $t('Backups'),
+            to: '/admin/zbackup/snapshots',
+        })
+        
+        menu.add({
+            id: 'zbackup-plans',
+            label: $t('Plans'),
+            icon: 'CalendarCheck',
+            group: $t('Backups'),
+            to: '/admin/zbackup/plans',
+        })
+    }
+}
